0700bda780217e186912335f6d7f758441b3b53a
juanexpensive <carovaquerojuan@gmail.com>
2025-10-01 14:34:52 +0200

    .net web forms

---
+++ unit5/HelloWorldSLN/HelloWorld/Default.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="HelloWorld._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        <asp:Label ID="lblNombre" runat="server" Text="Nombre:"></asp:Label>
+        <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox>
+        <asp:Label ID="lblError" runat="server" ForeColor="Red"></asp:Label>
+    </p>
+    <p>
+        <asp:Button ID="Button1" runat="server" Text="Saludar" OnClick="Button1_Click" />
+    </p>
+    <p>
+        <asp:Label ID="lblSaludo" runat="server" Font-Size="Large"></asp:Label>
+    </p>
+</asp:Content>
---
+++ unit5/HelloWorldSLN/HelloWorld/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace HelloWorld
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+
+            if (string.IsNullOrEmpty(nombre))
+            {
+                lblError.Text = "Debe introducir un nombre.";
+                lblSaludo.Text = "";
+            }
+            else
+            {
+                lblError.Text = "";
+                lblSaludo.Text = "Hola " + nombre;
+            }
+        }
+    }
+}
