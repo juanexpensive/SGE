fdd384c9438c6155d1be1589757ef9dee302a8e4
juanexpensive <carovaquerojuan@gmail.com>
2025-10-08 14:40:49 +0200

    starting in mvc

--- unit6/01HelloWorld/01HelloWorld/obj/Debug/net9.0/01HelloWorld.AssemblyInfo.cs
+++ unit6/01HelloWorld/01HelloWorld/obj/Debug/net9.0/01HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75cec235a6d34057e8336cbb8313ce74a4401ae8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e0eb9155335217e82c1b4237d6256b1bff0ee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("01HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
