8b9c582a818d10c6f861250fa7d415b51ec56987
juanexpensive <carovaquerojuan@gmail.com>
2025-11-03 14:41:31 +0100

    empezao boletin 1

---
+++ unit8/Formulario/Formulario/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Formulario.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Formulario.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Saludo(String nombre)
+        {
+            ViewBag.nombre = nombre;
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ unit8/Formulario/Formulario/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Person() { }
+        
+        public Person(int id, string name, string surname, DateTime dateOfBirth)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            DateOfBirth = dateOfBirth;
+        }
+    }
+}
